313b4a7d44ae84c791e07b71706e8fb57145009b
Kamel Arab <kamel.arab@fia-net.com>
2014-08-28 17:35:41 +0200

    Review

--- src/ng-addPub.js
+++ src/ng-addPub.js
@@ -9,41 +9,47 @@ angular.module('ng-addPub',[])
 
     .directive('addPub',
     function($compile, $document) {
-        function link(scope, element,attrs) {
-            scope.title = attrs.title || "Default";
-            console.log("IN Link");
-            var topWidthHeader = element[0].children[0].offsetWidth;
-            console.log("topWidthHeader : "+ topWidthHeader);
-            var topHeightHeader = element[0].children[0].offsetHeight;
-            console.log("topHeightHeader : "+ topHeightHeader);
-            var rigthPositionHeader = topWidthHeader/2-topHeightHeader/2
-            console.log("rigthPositionBody : "+ rigthPositionBody);
-            var rigthPositionBody = element[0].children[1].offsetWidth
-            element[0].style.right ="-"+rigthPositionBody+"px"
-            var classString = element[0].className
+        function compile(tElement, tAttrs, transclude) {
+            return {
+                pre: function preLink(scope, iElement, iAttrs, controller) {
 
-            scope.toggleOpen = function(){
+                    scope.title = iAttrs.title || "Open" ;
+                    if(angular.isDefined(iAttrs.addIconClass)){
+                        scope.addIcon = true;
+                        scope.addIconClass = iAttrs.addIconClass;
+                    }
+                },
+                post: function postLink(scope, iElement, iAttrs, controller) {
 
-                if(element[0].classList.contains('add-pub-open')){
-                    nameIndex = classString.indexOf('add-pub-open');
-                    classString = classString.substr(0, nameIndex) + classString.substr(nameIndex+'add-pub-open'+length +1) + 'add-pub-close';
-                    element[0].className = classString
+                    var rigthPositionBody = iElement[0].children[1].offsetWidth
+                    iElement[0].style.right ="-"+rigthPositionBody+"px"
+                    var restHeigth = iElement[0].clientHeight -200 ;
+                    iElement[0].children[0].style.marginTop =  restHeigth/2 +"px";
+                    var classString = iElement[0].className;
+                    scope.toggleOpen = function(){
+                        var classString = iElement[0].className;
+                        if(iElement[0].classList.contains('add-pub-open')){
+                            nameIndex = classString.indexOf('add-pub-open');
+                            classString = classString.substr(0, nameIndex) + classString.substr(nameIndex+'add-pub-open'.length +1) + 'add-pub-close';
+                            iElement[0].className = classString
 
-                }else{
-                    nameIndex = classString.indexOf('add-pub-close');
-                    classString = classString.substr(0, nameIndex) + classString.substr(nameIndex+'add-pub-open'.length +1) + 'add-pub-open';
-                    element[0].className = classString
+                        }else{
+                            nameIndex = classString.indexOf('add-pub-close');
+                            classString = classString.substr(0, nameIndex) + classString.substr(nameIndex+'add-pub-close'.length +1) + 'add-pub-open';
+                            iElement[0].className = classString
+                        }
+                    }
                 }
             }
         }
         return {
             restrict: 'E',
             replace: true,
-            link: link,
+            compile: compile,
             transclude: true,
             template: '<div class="addPub add-pub-close">' +
-                        '<div class="header" ng-click="toggleOpen()"><ul><li ng-repeat= "l in title.split(\'\') track by $index">{{l}}</li></ul></div>' +
-                        '<div class="body" > <div ng-transclude></div></div>' +
+                        '<div class="header" ng-click="toggleOpen()"><ul><li ng-if="addIcon"><span class="{{addIconClass}}"></span><li ng-repeat= "l in title.split(\'\') track by $index">{{l}}</li></ul></div>' +
+                        '<div class="body" > <div class="pre-scrollable"> <div ng-transclude></div></div></div>' +
                       '</div>'
         };
     });
